036410535622688fc19d4f45d4491ddd21fd3c65
ilyass.elfouih <ilyass.elfouih@gmail.com>
2022-08-18 17:58:53 +0100

    style(workspace): :rotating_light: fromatting all files with prettier

--- src/app/chat/chat-header/chat-header.component.spec.ts
+++ src/app/chat/chat-header/chat-header.component.spec.ts
@@ -8,9 +8,8 @@ describe('ChatHeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChatHeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [ChatHeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ChatHeaderComponent);
     component = fixture.componentInstance;
